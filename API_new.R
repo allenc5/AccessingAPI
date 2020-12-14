2a07c8af8ce8c90ba029b4781ec36a9a09e7f11b
allenc5 <61200323+allenc5@users.noreply.github.com>
2020-12-14 23:46:45 +0000

    Extracted data from my gothub account aswell as Fabien Potercier's account

--- API_new.R
+++ API_new.R
@@ -33,3 +33,24 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 gitDF[gitDF$full_name == "allenc5/datasharing", "created_at"] 
 
 # The above code was sourced from https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08  
+
+#Interrogate the Github API to extract data from my own github account
+myData = fromJSON("https://api.github.com/users/allenc5")
+myData$followers #dispalys number of followers
+myData$following #displays number of people I am following
+myData$public_repos #displays the number of repositories I have
+myData$bio #Displays my bio
+
+#Interrogate the Github API to extract data from another account by switching the username
+fabpotData = fromJSON("https://api.github.com/users/fabpot")
+fabpotData$followers
+fabpotData$following 
+fabpotData$public_repos 
+fabpotData$bio 
+
+
+
+
+
+
+
